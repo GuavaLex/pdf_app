fdfdbeb12ddcd6ed738d110c03a970ddfad0f86d
guava <guavareigen2@gmail.com>
2023-02-15 01:35:00 +0530

    Added footer and fixed the code

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation = "P", unit = "mm", format = "A4")
+pdf.set_auto_page_break(auto=0,margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    for i in range(row["Pages"]):
+
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L")
+    pdf.line(10,27,200,27)
+
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+
+    for i in range(row["Pages"]-1):
         pdf.add_page()
-        pdf.set_font(family="Times",style="B", size=12)
-        pdf.set_text_color(100,100,100)
-        if i==0:
-            pdf.cell(w=0, h=12, txt=row["Topic"], align="L",ln=1)
-            pdf.line(10,22,200,22)
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=8, txt=row["Topic"], align="R")
+
 
 
 pdf.output("output.pdf")
